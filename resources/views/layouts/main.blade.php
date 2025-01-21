2a618c60c28e56a094249c13e2769143244aaee4
Piyan0 <piyanapriyanto.0@gmail.com>
2025-01-21 15:34:45 +0700

    initial_3

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,9 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-  <link rel="stylesheet" href="bootstrap@5.2.0.min.css">
-  <link rel="stylesheet" href="p_utility_classes.css">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://piyan0.github.io/quotes_anime/bootstrap@5.2.0.min.css">
+  <link rel="stylesheet" href="https://piyan0.github.io/quotes_anime/host_style.css">
 
   <title>EventManagement</title>
 </head>
@@ -45,7 +44,54 @@
 
     </div>
   </div>
+<style>
+  body {
+    font-size: 15pt;
+}
 
+.side-icon{
+  filter: brightness(0) saturate(100%) invert(49%) sepia(14%) saturate(4678%) hue-rotate(204deg) brightness(100%) contrast(91%);
+}
+
+#sidebar-link div:active{
+  background-color: #F1F4F9
+}
+
+.purple{
+  filter: brightness(0) saturate(100%) invert(46%) sepia(24%) saturate(4076%) hue-rotate(212deg) brightness(103%) contrast(99%);
+}
+
+.white-filt{
+  filter: brightness(0) saturate(100%) invert(95%) sepia(5%) saturate(0%) hue-rotate(53deg) brightness(105%) contrast(105%);
+}
+
+.shade-grey-filt{
+  filter: brightness(0) saturate(100%) invert(74%) sepia(8%) saturate(749%) hue-rotate(198deg) brightness(84%) contrast(90%);
+  }
+  
+.phone-sidebar-icon{
+  filter: brightness(0) saturate(100%) invert(35%) sepia(14%) saturate(2425%) hue-rotate(191deg) brightness(92%) contrast(82%);
+}
+
+
+.mobile_nav_active{
+  display: block!important
+}
+
+
+.mobile_nav_active_p{
+  font-weight: bold;
+  color: #00639C!important;
+}
+a{
+  text-decoration: none;
+}
+a p{
+  color: black;
+}
+
+
+</style>
 </body>
 
 </html>
